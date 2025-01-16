e8b3feafdceb49c134db68c226ed10d637a0849e
HahanDmytr <hahandmytro@gmail.com>
2025-01-16 20:09:38 +0100

    change some-api-key

--- src/components/donate/stripe.js
+++ src/components/donate/stripe.js
@@ -1,6 +1,6 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-const publishedKey = process.env.REACT_APP_PUBLISHED_KEY;
+const publishedKey = process.env.REACT_APP_PUBLISHED_STRIPEKEY;
 
 
 export const stripePromise = loadStripe(publishedKey);
